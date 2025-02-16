2dd38d43b8b3e728154a9e67edb8286d2ca10175
jk <2676748512@qq.com>
2025-02-16 17:41:18 +0800

    404

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ?
-        '/parrot-world/' // 替换成你的仓库名
+        '/dayingwu/' // 替换成你的仓库名
         :
         '/',
     outputDir: 'dist'
