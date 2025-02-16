b3e901bf2073b2409adab7ba59108ce14fa8de0d
jk <2676748512@qq.com>
2025-02-16 18:10:08 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 //import { createApp } from 'vue'
-const { createApp } = Vue;
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
